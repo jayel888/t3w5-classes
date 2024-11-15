4d2f366fde2f6f6ef70bc3739dd49d3a00f4453b
jayel888 <15644@coderacademy.edu.au>
2024-11-16 10:02:44 +1100

    feat: Example Dog Class

---
+++ jsclasses/app.js
@@ -0,0 +1,12 @@
+class Dog {
+    constructor(name, breed, age, color){
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+        this.color = color;
+    }
+}
+
+let brockie = new Dog('Brockie', 'German Shepherd', '3', 'Brown');
+
+console.log(brockie);
