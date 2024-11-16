1ed2fb4741bbf6c22bb1b4e246e2193f281cb9a1
jayel888 <15644@coderacademy.edu.au>
2024-11-16 12:56:09 +1100

    feat: fetch 6 pokemon using Lifecycle components and this.setState method

--- pokemon-class-demo/src/PokemonFetcher.jsx
+++ pokemon-class-demo/src/PokemonFetcher.jsx
@@ -12,14 +12,24 @@ export default class PokemonFetcher extends Component {
         }
     }
 
-    async componentDidMount(){
-        // Generate a random Pokemon ID Number
-        let randomNumber = Math.ceil(Math.random() * 1025);
-        // Pass random Pokemon Number to fetch
-        let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`)
-        let data = await response.json();
-        // set fetch response data to the state
-        this.setState({pokemonList: [data.name]});
+    async componentDidMount() {
+        for (let index = 0; index < 6; index++) {
+            // Generate a random Pokemon ID Number
+            let randomNumber = Math.ceil(Math.random() * 1025);
+            // Pass random Pokemon Number to fetch
+            let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`)
+            let data = await response.json();
+            // set fetch response data to the state
+            // Method 1
+            // this.setState({ pokemonList: [...this.state.pokemonList, data.name] });
+
+            // Method 2
+            this.setState((previousState) => {
+                return {
+                    pokemonList: [...previousState.pokemonList, data.name]
+                }
+            })
+        }
         console.log("PokemonFetcher first load on the page");
     }
 
@@ -33,7 +43,7 @@ export default class PokemonFetcher extends Component {
                     })
                 }
                 <button onClick={() => {
-                    this.setState({pokemonList: []});
+                    this.setState({ pokemonList: [] });
                 }}>Empty the state</button>
             </div>
         )
